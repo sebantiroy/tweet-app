227a1b30eaf200c7a2e352007977467c3aaa4254
sebantiroy <sebantiroy143@gmail.com>
2022-09-15 15:01:55 +0530

    added

--- src/Components/bootApi.js
+++ src/Components/bootApi.js
@@ -1,2 +1,2 @@
-const base_url="http://ec2-3-82-115-69.compute-1.amazonaws.com:8083/api/v1.0/tweets";
+const base_url="http://ec2-3-87-205-137.compute-1.amazonaws.com:8083/api/v1.0/tweets";
 export default base_url;
